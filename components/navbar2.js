66e2bce7c3a5dc3134decee6d617bf54bddc73f8
RakeshAhire <rocky5509ahire@gmail.com>
2022-08-27 23:31:34 +0530

    Navbar 2 added

---
+++ components/navbar2.js
@@ -0,0 +1,36 @@
+function mynavbar(){
+    return `<div id="navbar2">
+    <div class="left2">
+        <img src="logo.png" alt="">
+        <p>For Entrepreneur </p>
+        <select name="" id="">
+            <option value="" hidden>How it works</option>
+            <option value="">Campaign</option>
+            <option value="">In Demand</option>
+            <option value="">China Program</option>
+            <option value="">Fees</option>
+            <option value="">Community</option>
+        </select>
+        <select name="" id="">
+            <option value="" hidden>Resources</option>
+            <option value="">Education Center</option>
+            <option value="">Expert Directory</option>
+            <option value="">Help & Support</option>
+            <option value="">Trust & Safety</option>
+            
+        </select>
+        <select name="" id="">
+            <option value="" hidden>About Us</option>
+            <option value="">Indiegogo vs Kickstarter</option>
+            <option value="">Press</option>
+            <option value="">About us</option>               
+        </select>
+    </div>
+    <div class="right2">
+        <button>Start a Campaign</button>
+        <h3>Log In</h3>
+        <h3>Sign Up</h3>
+    </div>
+</div>`
+}
+export default mynavbar;
